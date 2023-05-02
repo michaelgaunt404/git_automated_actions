f89ae24816e0578f77089e7bc31e77712a5aa5e7
mike gaunt <merkaberk91@yahoo.com>
2023-05-02 09:41:35 -0700

    factory reset

---
+++ send_email.R
@@ -0,0 +1,20 @@
+# Load blastula package
+library(blastula)
+
+print("PRINTING")
+
+# Create file containing credentials for email account
+# create_smtp_creds_file(file = "mgthrow",
+#                        user = "gaunt_throwaway_123@outlook.com",
+#                        provider = "outlook",
+#                        use_ssl = TRUE)
+
+# Generate the email object using blastula
+my_email_object <- render_email("blastula_email.rmd")
+
+# Send email
+smtp_send(my_email_object,
+          from = "gaunt_throwaway_123@outlook.com",
+          to = "merkaberk91@yahoo.com",
+          subject = paste0("Email from blastula on ", Sys.Date()),
+          credentials = creds_file("mgthrow"))
